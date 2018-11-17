212293ea613e08d1f240e0e5e88ad9de89a70b70
Unknown <monkeymanp8@gmail.com>
2018-11-16 19:48:59 -0500

    Added Tiles back

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -4,77 +4,40 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
     public float walkSpeed = 10.0f;
-    public float velocity;
-    public float gscale; 
     bool facingRight = true;
     SpriteRenderer spriterenderer;
-    Animator animator;
-    Rigidbody2D rb; 
-
+    Animator animator; 
     // Use this for initialization
     void Start () {
         spriterenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>(); 
+        animator= GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Walk();
-        velocity = rb.velocity.y;
-        gscale = rb.gravityScale; 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.velocity += new Vector2(0, 9.0f);
-            animator.SetBool("isJumping", true);
-        }
-
-        if (rb.velocity.y <= 0)
-        {
-            animator.SetBool("isJumping", false);
-        }
-
-        if (rb.velocity.y < 0)
-        {
-            rb.gravityScale = 6;
-            animator.SetBool("isFalling", true); 
-        }
-	}
-
-    void Walk()
-    {
+        
         float walkTranslation = Input.GetAxis("Horizontal") * walkSpeed * Time.deltaTime;
 
-        transform.Translate(walkTranslation, 0, 0);
-        if (walkTranslation < 0 && facingRight)
+        transform.Translate(walkTranslation, 0, 0); 
+        if(walkTranslation < 0 && facingRight)
         {
             Flip();
+            animator.SetBool("isWalking", true); 
         }
         else if (walkTranslation > 0 && !facingRight)
         {
             Flip();
-        }
-
-        if (walkTranslation == 0)
-        {
-            animator.SetBool("isWalking", false);
-        }
-        else
-        {
             animator.SetBool("isWalking", true);
         }
-    }
+        else if(walkTranslation == 0)
+        {
+            animator.SetBool("isWalking", false); 
+        }
+	}
 
     void Flip()
     {
         facingRight = !facingRight;
         spriterenderer.flipX = !spriterenderer.flipX; 
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        rb.gravityScale = 2;
-        animator.SetBool("isFalling", false); 
-        animator.SetBool("onGround", true); 
     }
 }
