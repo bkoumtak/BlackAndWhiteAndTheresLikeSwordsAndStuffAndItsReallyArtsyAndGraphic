2923bf16aa2386b1561e017b388e1fd7c3361ada
bkoumtak <bkoumtak@gmail.com>
2018-11-18 23:01:37 -0500

    Added basic attack chain combo
    Implemented attack system

---
+++ Assets/Scripts/Destroy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+    public float seconds = 0.2f; 
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, seconds);
+    }
+}
