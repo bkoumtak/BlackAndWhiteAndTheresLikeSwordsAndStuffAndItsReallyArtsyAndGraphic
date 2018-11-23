d04d2c5b421c7849a7f242f21531bcb3f4ea2d73
bkoumtak <bkoumtak@gmail.com>
2018-11-23 04:03:16 -0500

    Finished color swapping, eye syncing as well as smoothed attack

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,21 +10,26 @@ public class Player : MonoBehaviour
     public float gscale;
     public Transform jumpCloud;
     public Transform[] slashes;
-    bool onGround = false;
+    public Transform eyes; 
+    public bool onGround = false;
     bool isAttacking = false; 
     bool facingRight = true;
     public bool isWhite = true; 
 
     SpriteRenderer spriterenderer;
+    SpriteRenderer eye_renderer; 
     Animator animator;
+    Animator eye_animator;
     Rigidbody2D rb;
 
     // Use this for initialization
     void Start()
     {
         spriterenderer = GetComponent<SpriteRenderer>();
+        eye_renderer = eyes.GetComponent<SpriteRenderer>(); 
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        eye_animator = eyes.GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
@@ -39,19 +44,24 @@ public class Player : MonoBehaviour
             onGround = false; 
             rb.velocity += new Vector2(0, 15.0f);
             animator.SetBool("isJumping", true);
+            eye_animator.SetBool("isJumping", true);
+            eye_animator.SetBool("isRunning", false); 
             changeObjectColor(jumpCloud); 
-            Instantiate(jumpCloud, transform.position, Quaternion.identity); 
+            Instantiate(jumpCloud, transform.position, Quaternion.identity);
+          
         }
 
         if (rb.velocity.y <= 0)
         {
             animator.SetBool("isJumping", false);
+            eye_animator.SetBool("isJumping", false); 
         }
 
         if (rb.velocity.y < 0)
         {
             rb.gravityScale = 10;
             animator.SetBool("isFalling", true);
+            eye_animator.SetBool("isFalling", true); 
         }
 
         /* Determines the form of the player */
@@ -68,11 +78,11 @@ public class Player : MonoBehaviour
     void Walk()
     {
         float walkTranslation = 0.0f;
-        
-        if (isAttacking && onGround)
+
+        if (isAttacking && (rb.velocity.y == 0))
         {
-            walkTranslation = 0.0f; 
-        } 
+            walkTranslation = 0.0f;
+        }
         else
         {
             walkTranslation = Input.GetAxis("Horizontal") * walkSpeed * Time.deltaTime;
@@ -91,10 +101,16 @@ public class Player : MonoBehaviour
         if (walkTranslation == 0)
         {
             animator.SetBool("isWalking", false);
+            eye_animator.SetBool("isRunning", false); 
         }
         else
         {
-            animator.SetBool("isWalking", true);
+            if (onGround)
+            {
+                animator.SetBool("isWalking", true);
+                eye_animator.SetBool("isRunning", true);
+            }
+           
         }
     }
 
@@ -105,35 +121,46 @@ public class Player : MonoBehaviour
             animator.ResetTrigger("Attack2");
             animator.ResetTrigger("Attack1");
             animator.ResetTrigger("Attack3");
+
+            eye_animator.ResetTrigger("Attack2");
+            eye_animator.ResetTrigger("Attack1");
+            eye_animator.ResetTrigger("Attack3");
             isAttacking = false; 
         }
 
         if (Input.GetKeyDown(KeyCode.J)) {
-            isAttacking = true; 
-            float rotation = 0; 
-            if (facingRight)
+            if (onGround)
             {
-                rotation = 0f; 
-            }
-            else
-            {
-                rotation = 180f; 
-            } 
+                isAttacking = true;
+                float rotation = 0;
+                if (facingRight)
+                {
+                    rotation = 0f;
+                }
+                else
+                {
+                    rotation = 180f;
+                }
 
-            if (animator.GetCurrentAnimatorStateInfo(0).IsName("White-Idle-Sword")) { 
-                animator.SetTrigger("Attack1");
-            }
-            else if (animator.GetCurrentAnimatorStateInfo(0).IsName("White-Attack-1"))
-            {
-                animator.SetTrigger("Attack2");
-                changeObjectColor(slashes[1]);
-                Object.Instantiate(slashes[1], transform.position, Quaternion.Euler(0, rotation, 0));
-            }
-            else if (animator.GetCurrentAnimatorStateInfo(0).IsName("White-Attack-2"))
-            {
-                animator.SetTrigger("Attack3");
-                changeObjectColor(slashes[2]);
-                Object.Instantiate(slashes[2], transform.position, Quaternion.Euler(0, rotation, 0));
+                if (animator.GetCurrentAnimatorStateInfo(0).IsName("White-Idle-Sword") || animator.GetCurrentAnimatorStateInfo(0).IsName("White-Run"))
+                {
+                    animator.SetTrigger("Attack1");
+                    eye_animator.SetTrigger("Attack1");
+                }
+                else if (animator.GetCurrentAnimatorStateInfo(0).IsName("White-Attack-1"))
+                {
+                    animator.SetTrigger("Attack2");
+                    eye_animator.SetTrigger("Attack2");
+                    changeObjectColor(slashes[1]);
+                    Object.Instantiate(slashes[1], transform.position, Quaternion.Euler(0, rotation, 0));
+                }
+                else if (animator.GetCurrentAnimatorStateInfo(0).IsName("White-Attack-2"))
+                {
+                    animator.SetTrigger("Attack3");
+                    eye_animator.SetTrigger("Attack3");
+                    changeObjectColor(slashes[2]);
+                    Object.Instantiate(slashes[2], transform.position, Quaternion.Euler(0, rotation, 0));
+                }
             }
         }
     }
@@ -173,6 +200,7 @@ public class Player : MonoBehaviour
     {
         facingRight = !facingRight;
         spriterenderer.flipX = !spriterenderer.flipX;
+        eye_renderer.flipX = !eye_renderer.flipX; 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -180,6 +208,8 @@ public class Player : MonoBehaviour
         onGround = true; 
         rb.gravityScale = 2;
         animator.SetBool("isFalling", false);
+        eye_animator.SetBool("isFalling", false); 
         animator.SetBool("onGround", true);
+        eye_animator.SetBool("onGround", true); 
     }
 }
